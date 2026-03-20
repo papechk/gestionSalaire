50628799a161adb5ff474e6bd3629fbf57edcfd2
pkeinde6 <pkeinde6@gmail.com>
2026-03-20 13:47:09 -0400

    chore: update admin email to admin@example.com

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::firstOrCreate(
-            ['email' => 'admin@gestionsalaire.com'],
+            ['email' => 'admin@example.com'],
             ['name' => 'Administrateur', 'password' => Hash::make('password')]
         );
 
